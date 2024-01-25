db096c8b547f7b6a4dba62229b9579dd63ea4fbd
victorC95 <victorcorreaa95@gmail.com>
2024-01-25 17:25:14 -0300

    Improvements

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
 import { EXAMPLES } from "../data";
 
 export default function Examples() {
@@ -26,8 +27,7 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
@@ -55,6 +55,6 @@ export default function Examples() {
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton ({children, onClick, isSelected}){
-    return (
-        <li>
-            <button className={isSelected ? 'active' : undefined} onClick = {onClick}>{children}</button>
-        </li>
-    );
-};
+export default function TabButton({ children, isSelected, ...props }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
+    </li>
+  );
+}
